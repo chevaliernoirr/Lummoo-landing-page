43cfc87f63245dbb4198608d8e2802fb144a31b3
Dário Andrade <darioking.andrade@gmail.com>
2025-11-29 00:05:03 +0000

    Atualizar o App.tsx

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import ProblemSection from './components/ProblemSection';
-import Calculator from './components/Calculator';
-import BenefitsSection from './components/BenefitsSection';
-import ComparisonSection from './components/ComparisonSection';
-import AudienceSection from './components/AudienceSection';
-import Footer from './components/Footer';
+import Header from './components/Header.tsx';
+import Hero from './components/Hero.tsx';
+import ProblemSection from './components/ProblemSection.tsx';
+import Calculator from './components/Calculator.tsx';
+import BenefitsSection from './components/BenefitsSection.tsx';
+import ComparisonSection from './components/ComparisonSection.tsx';
+import AudienceSection from './components/AudienceSection.tsx';
+import Footer from './components/Footer.tsx';
 
 const App: React.FC = () => {
   return (
